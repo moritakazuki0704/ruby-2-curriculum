465dd6abfd8d74261bee92939b33c9e4a428ab77
EC2 Default User <ec2-user@ip-172-31-33-118.ec2.internal>
2022-06-20 02:25:59 +0000

    [finish]ruby-6 curriculum [add]実証自習

--- amount.rb
+++ amount.rb
@@ -12,3 +12,7 @@ amounts = {"リンゴ" =>2 , "いちご" => 5,"オレンジ"=>3}
 amounts.each do |fruit,amount|
   puts "#{fruit}は#{amount}個です。"
 end
+
+{"赤"=>7,"青"=>8,"黄"=>11}.each do |color,red|
+  puts "#{color}色は#{red}角です。"
+end

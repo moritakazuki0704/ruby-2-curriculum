5107cec6e0b9d92db6f66005c7504d754a29710a
EC2 Default User <ec2-user@ip-172-31-33-118.ec2.internal>
2022-06-21 21:27:42 +0000

    [finish]ruby-9 curriculum

--- new.rb
+++ new.rb
@@ -11,3 +11,32 @@ car.run(5)
 
 puts "そこの交差点で"
 car.turn("右")
+
+class Car
+  def self.run(distance)
+    puts "車で#{distance}キロが制限速度です。"
+  end
+  def Car.turn(direction)
+    puts "#{direction}に曲がります"
+  end
+end
+
+Car.run(10)
+car.run(8)
+Car.turn("左")
+
+class Bike
+  def move(direction,distance)
+    self.turn(direction)
+    self.run(distance)
+  end
+  def turn(direction)
+    puts "二段階#{direction}折"
+  end
+  def run(distance)
+    puts "#{distance}キロで走ります"
+  end
+end
+
+bike=Bike.new
+bike.move("右",3)
